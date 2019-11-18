d9c13b04688f6d1316d98de22bd02836178f39a1
Oliver Günther <mail@oliverguenther.de>
2019-11-18 14:14:42 +0100

    Use internal URL for CSP

--- modules/ifc_models/app/helpers/ifc_models_helper.rb
+++ modules/ifc_models/app/helpers/ifc_models_helper.rb
@@ -1,9 +1,5 @@
 module IFCModelsHelper
   def attachment_content_url(attachment)
-    if attachment.external_storage?
-      attachment.external_url
-    else
-      API::V3::Utilities::PathHelper::ApiV3Path.attachment_content(attachment.id)
-    end
+    API::V3::Utilities::PathHelper::ApiV3Path.attachment_content(attachment.id)
   end
 end
